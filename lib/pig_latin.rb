c7b5521c623dcbc3bbf7a719eba899d51516c0c6
Dan Murphy <murphydbuffalo@gmail.com>
2014-06-11 13:39:06 -0400

    Added functionality for translating words that begin with some number of consonants.

--- lib/pig_latin.rb
+++ lib/pig_latin.rb
@@ -15,10 +15,23 @@ class PigLatinTranslation
     end
 
     vowels = ['a', 'e', 'i', 'o', 'u']
-
+    position = 100
+    consonants = nil
     if vowels.any? {|vowel| string[0].downcase == vowel}
-    	puts string
     	word = ("#{string}way").capitalize 
+    else
+    	#Find first vowel in the word
+    	vowels.each do |v| 
+    	  if string.include?(v)	
+    		  position = string.index(v) if position > string.index(v)
+    	  end
+    	end
+    	
+    	consonants = string[0..(position - 1)]
+    	
+    	new_string = "#{string[position..(string.length - (position) )]}#{consonants}" #Move preceding letters to the end of the word 
+    	
+    	word = ("#{new_string}ay").capitalize #Add "ay" to end of the word 
     end
     word
   end
@@ -34,6 +47,5 @@ end
 
 puts "Please enter a word:"
 input = PigLatinTranslation.new(gets.chomp)
-
 puts input.translate
 
